121e8809c11fb2cb0c707bd664570662c322e9b5
lsdeu1 <767621250@qq.com>
2021-07-06 14:47:51 +0800

    070602

--- cantrans1.cpp
+++ cantrans1.cpp
@@ -28,28 +28,47 @@ return strDestCopy;
 int main(int argc, char **argv)
 {
     int s, nbytes;                                //socket套接字和发送字节数目
-    struct sockaddr_can addr;                     //设置用结构体
-    struct ifreq ifr;                             //指定can设备用结构体   
-    struct can_frame frame;                       //接收数据的buffer
-    struct can_filter rfilter[1];                 //过滤用的结构体
+    struct sockaddr_can addr;                     //设置结构体
+    struct ifreq ifr;                             //指定can设备结构体   
+    struct can_frame frame;                       //send-buffer
+    struct can_filter rfilter[1];                 //过滤结构体
     s = socket(PF_CAN, SOCK_RAW, CAN_RAW);        //创建套接字
+
     strcpy(ifr.ifr_name, "vcan0" );                
-    ioctl(s, SIOCGIFINDEX, &ifr);                 //指定 vcan0 设备
+    ioctl(s, SIOCGIFINDEX, &ifr);                 //指定 vcan0
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
     bind(s, (struct sockaddr *)&addr, sizeof(addr)); //将套接字与 vcan0 绑定
-
-    rfilter[0].can_id =0x11;                        //定义接收规则，只接收表示符等于 0x11 的报文
+     
+    /*
+    struct can_filter rfilter[2];
+    rfilter[0].can_id   = 0x123;
     rfilter[0].can_mask = CAN_SFF_MASK;
-
+    rfilter[1].can_id   = 0x200;
+    rfilter[1].can_mask = 0x700;
+    */
+    rfilter[0].can_id =0x03;                        //ID
+    rfilter[0].can_mask = CAN_SFF_MASK;
     setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter)); //设置过滤规则
-    while(1)                                       //canbus消息
+    int flag=0;
+        for(int i=0;i<8;i++)
+        {
+            frame.data[i]=1;
+        };
+    while(1)                                       //canbus
     {
-    nbytes = write(s, &frame, sizeof(frame));       
-        if(nbytes > 0)
-         {
-        printf("ID=0x%X DLC=%d data[0]=0x%X\n", frame.can_id,frame.can_dlc, frame.data[0]);
-         }
+        cin >> flag;
+        if(flag!=0)
+        {
+            //write data in  frame.data
+            nbytes = write(s, &frame, sizeof(frame));       //sendcan
+            if(nbytes > 0)
+            {
+            printf("ID=0x-%X DLC=%d\n", frame.can_id,frame.can_dlc);
+            }
+            flag=0;
+        }
+        
     }
     close(s);
     return 0;
